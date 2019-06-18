3948b7d2d563a15d19b2b415b59ff913ea4f4493
duocore_c <lanzheng_1123@163.com>
2019-06-18 11:36:49 +0800

    1) add tcp server to simulate the p2p client. 2) connect server with domain name instead of fixed ip. 3) the tcp client no longer use a thread to connect server, used async connect install .

--- BlockDet/TestClient/TestClient.cpp
+++ BlockDet/TestClient/TestClient.cpp
@@ -2,6 +2,7 @@
 #include "AsioTCPClient.h"
 #include "boost/thread.hpp"
 #include "boost/shared_ptr.hpp"
+#include "boost/make_shared.hpp"
 
 using boost::asio::ip::udp;
 enum { max_length = 1024 };
@@ -51,16 +52,62 @@ private:
 	char m_receive_buffer[max_length];
 };
 
-void tcp_peer_connect(io_service& io)
+typedef ip::tcp::socket tcp_socket_type;
+typedef boost::shared_ptr<tcp_socket_type> tcp_socket_ptr;
+
+// this class is used to test `pingpong` task
+class async_tcp_server
 {
-	std::cout << "tcp client start." << std::endl;
-	CAsioTCPClient cl(io);
-	cl.start();
-	while (!cl.stoped())
+public:
+	async_tcp_server(boost::asio::io_service& io)
+		: m_io(io)
+		, m_acceptor(m_io, ip::tcp::endpoint(ip::tcp::v4(), 3799))
 	{
-		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+		accept();
 	}
-	std::cout << "tcp client finished." << std::endl;
+	~async_tcp_server() { ; }
+
+protected:
+private:
+	void accept()
+	{
+		tcp_socket_ptr sock = boost::make_shared<tcp_socket_type>(m_io);
+		m_acceptor.async_accept(*sock, boost::bind(&async_tcp_server::accept_handler, this, _1, sock));
+	}
+
+	void accept_handler(const boost::system::error_code& ec, tcp_socket_ptr sock)
+	{
+		if (ec)
+		{
+			return;
+		}
+		printf("SOCKET: %u Connected.\n", (unsigned int)sock->native_handle());
+		// we do not need to do anything in `pingpong` task because we just test whether it can be connected or not. Just close it after connection was built.
+		boost::system::error_code ec_unused;
+		sock->shutdown(socket_base::shutdown_both, ec_unused);
+		sock->close(ec_unused);
+		printf("Socket is closed.");
+		accept();
+	}
+private:
+	boost::asio::io_service& m_io;
+	ip::tcp::acceptor m_acceptor;
+};
+
+boost::shared_ptr<CAsioTCPClient> cc;
+
+void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it, io_service& io)
+{
+	if (!ec)
+	{
+		std::cout << "resolved ip: " << it->endpoint().address().to_string() << std::endl;
+		cc = boost::make_shared<CAsioTCPClient>(io, it->endpoint().address());
+		cc->start();
+	}
+	else
+	{
+		std::cout << "Failed to resolve." << std::endl;
+	}
 }
 
 int main()
@@ -68,8 +115,13 @@ int main()
 	try
 	{
 		io_service io;
-		boost::thread thd(tcp_peer_connect, boost::ref(io));
+		std::cout << "tcp client start." << std::endl;
+		boost::asio::ip::tcp::resolver resolver(io);
+		boost::asio::ip::tcp::resolver::query query("pe.joy189.com", "8888");
+		resolver.async_resolve(query, boost::bind(resolve_handler, _1, _2, boost::ref(io)));
+
 		async_udp_server udp_server(io);
+		async_tcp_server tcps_server(io);
 		udp_server.run();
 		io.run();
 	}
