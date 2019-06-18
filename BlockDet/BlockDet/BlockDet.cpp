68ba5345d408a17c70d7317aa5a6af0c160613b3
duocore_c <lanzheng_1123@163.com>
2019-06-18 09:59:21 +0800

    Remove chars utf-8 can not show normally
    Signed-off-by: duocore_c <lanzheng_1123@163.com>

--- BlockDet/BlockDet/BlockDet.cpp
+++ BlockDet/BlockDet/BlockDet.cpp
@@ -19,12 +19,9 @@ LONG WINAPI DumpMiniDump(PEXCEPTION_POINTERS excpInfo)
 		0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	MINIDUMP_EXCEPTION_INFORMATION eInfo;
-	eInfo.ThreadId = GetCurrentThreadId(); //����Ҫ����Ϣ���ȥ
+	eInfo.ThreadId = GetCurrentThreadId();
 	eInfo.ExceptionPointers = excpInfo;
 	eInfo.ClientPointers = FALSE;
-
-	// ����, ����Dump. 98��֧��
-	// Dump��������С�͵�, ��ʡ�ռ�. ���Բο�MSDN���ɸ���ϸ��Dump.
 	MiniDumpWriteDump(
 		GetCurrentProcess(),
 		GetCurrentProcessId(),
@@ -46,9 +43,7 @@ int main()
 #ifdef _WIN32
 	SetUnhandledExceptionFilter(DumpMiniDump);
 #endif
-	//
-	//����3999�˿�
-	//
+
 #ifdef LOG_ENABLED
 	DateTime dt;
 	std::stringstream ss;
@@ -67,66 +62,3 @@ int main()
 	
     return 0;
 }
-// 
-// /**
-// * �ͻ��˵�����
-// */
-// struct CLIENT_REQUEST
-// {
-// 	boost::asio::ip::address a;
-// 	boost::asio::ip::tcp::socket sock_tcp;	//TCP������
-// 	unsigned short tcp_port;
-// 	boost::asio::ip::udp::socket sock_udp;
-// 	unsigned short udp_port;
-// };
-// 
-// #include "boost/function.hpp"
-// #include <vector>
-// //
-// // һ���̣߳����ڴ����첽����tcp
-// //
-// bool exit_flag = false;
-// typedef boost::function<void()> AsyncConnectTask;
-// std::vector<AsyncConnectTask> tasks;
-// 
-// 
-// void peer_connection::async_connect_tcp(boost::asio::ip::address addr, unsigned short port)
-// {
-// 	// �����ǰ������������Ԥ�������TCP����������ִ��ʧ�ܡ�
-// 	if (out_of_quota())
-// 	{
-// 		int error_code = err_out_of_quote;
-// 
-// 	}
-// 	boost::asio::ip::tcp::socket s;
-// 	boost::asio::ip::tcp::endpoint ep(addr, port);
-// 	s.set_option(time_out, 3000);
-// 	boost::system::error_code ec;
-// 	s.async_connect(ep, boost::bind(&peer_connection::on_connection_complete, self(), _1));
-// 	m_connect_start = aux::time_now();
-// }
-// 
-// void new_tasks(CLIENT_REQUEST& cr)
-// {
-// 	return;
-// }
-// 
-// 
-// void thread_async_connect_tcp()
-// {
-// 	while (!exit_flag)
-// 	{
-// 		
-// 	}
-// 	//�ر�����δ�ͷŵ�����
-// 	return;
-// }
-// // һ���̣߳����ڴ����첽����UDP����
-// 
-// 
-// 
-// /**
-//  * ���ͻ��˵�socket���ӺͿͻ��˵������һ������ӳ�䡣
-//  * ����ʱ�����������Ͽ�������ӵ�ʱ�򣬽�
-//  */
-// typedef std::map<boost::asio::ip::tcp::socket, CLIENT_REQUEST> CLIENT_REQUEST_MAP;
