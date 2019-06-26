00583e047606c69154767b0865be9aa38663a1fc
duocore <duocore@localhost>
2019-06-27 01:19:47 +0800

    reformat code style with netbeans.

--- BlockDet/TestClient/AsioTCPClient.h
+++ BlockDet/TestClient/AsioTCPClient.h
@@ -5,128 +5,114 @@
 #include <iostream>
 
 using namespace boost::asio;
-class CAsioTCPClient
-{
-	typedef ip::tcp::socket socket_type;
-	typedef boost::shared_ptr<socket_type> sock_ptr;
+
+class CAsioTCPClient {
+    typedef ip::tcp::socket socket_type;
+    typedef boost::shared_ptr<socket_type> sock_ptr;
 public:
-	CAsioTCPClient(io_service& io, const boost::asio::ip::address& addr, int tid)
-		: m_ep(addr, 6688)
-		, m_io(io)
-		, m_tid(tid)
-	{
-		*(unsigned short*)m_send_buf = 9;
-		m_send_buf[2] = 'A';
-		m_send_buf[3] = 'M';
-		m_send_buf[4] = 'I';
-		m_send_buf[5] = 'B';
-		m_send_buf[6] = 0;
-		*(unsigned short*)(m_send_buf + 7) = 3799;
-		*(unsigned short*)(m_send_buf + 9) = 3799;
-		m_sock = boost::make_shared<socket_type>(m_io);
-		m_stoped = false;
-	}
 
-	void start()
-	{
-		m_sock->async_connect(m_ep, boost::bind(&CAsioTCPClient::connect_handler, this, _1));
-	}
+    CAsioTCPClient(io_service& io, const boost::asio::ip::address& addr, int tid)
+    : m_ep(addr, 6688)
+    , m_io(io)
+    , m_tid(tid) {
+        *(unsigned short*) m_send_buf = 9;
+        m_send_buf[2] = 'A';
+        m_send_buf[3] = 'M';
+        m_send_buf[4] = 'I';
+        m_send_buf[5] = 'B';
+        m_send_buf[6] = 0;
+        *(unsigned short*) (m_send_buf + 7) = 3799;
+        *(unsigned short*) (m_send_buf + 9) = 3799;
+        m_sock = boost::make_shared<socket_type>(m_io);
+        m_stoped = false;
+    }
 
-	bool stoped() const { return m_stoped; }
+    void start() {
+        m_sock->async_connect(m_ep, boost::bind(&CAsioTCPClient::connect_handler, this, _1));
+    }
+
+    bool stoped() const {
+        return m_stoped;
+    }
 private:
-	
 
-	void connect_handler(const boost::system::error_code& ec)
-	{
-		if (ec)
-		{
-			std::cout << "[" << m_tid << "]" << "connect failed with error:" << ec << "(" << ec.message() << ")" << std::endl;
-			close();
-			return;
-		}
-		std::cout << "[" << m_tid << "]" << "send to:" << m_sock->remote_endpoint().address() << ":" << m_sock->remote_endpoint().port() << std::endl;
-		m_sock->async_send(buffer(m_send_buf), boost::bind(&CAsioTCPClient::send_handler, this, _1, _2));
-	}
+    void connect_handler(const boost::system::error_code& ec) {
+        if (ec) {
+            std::cout << "[" << m_tid << "]" << "connect failed with error:" << ec << "(" << ec.message() << ")" << std::endl;
+            close();
+            return;
+        }
+        std::cout << "[" << m_tid << "]" << "send to:" << m_sock->remote_endpoint().address() << ":" << m_sock->remote_endpoint().port() << std::endl;
+        m_sock->async_send(buffer(m_send_buf), boost::bind(&CAsioTCPClient::send_handler, this, _1, _2));
+    }
 
-	void recv_handler(
-		const boost::system::error_code& error, // Result of operation.
-		std::size_t bytes_transferred           // Number of bytes received.
-		)
-	{
-		if (error || bytes_transferred == 0)
-		{
-			std::cout << "[" << m_tid << "]" << "error: " << error << ". Bytes: " << bytes_transferred << std::endl;
-			close();
-			return;
-		}
+    void recv_handler(
+            const boost::system::error_code& error, // Result of operation.
+            std::size_t bytes_transferred // Number of bytes received.
+            ) {
+        if (error || bytes_transferred == 0) {
+            std::cout << "[" << m_tid << "]" << "error: " << error << ". Bytes: " << bytes_transferred << std::endl;
+            close();
+            return;
+        }
 
-		if (bytes_transferred == 3)
-		{
-			std::cout << "[" << m_tid << "]" << "We have received the respond." << std::endl;
-			unsigned char reply_code = m_buf[2];
-			std::cout << "[" << m_tid << "]" << "TCP:" << ((reply_code & 0x01) == 0 ? "Failed." : "Succeeded.") << std::endl;
-			std::cout << "[" << m_tid << "]" << "UDP:" << ((reply_code & 0x02) == 0 ? "Failed." : "Succeeded.") << std::endl;
-			std::cout << "[" << m_tid << "]" << "Socket is closing now." << std::endl;
-			close();
-		}
-		else
-		{
-			std::cout << "[" << m_tid << "]" << "Failed to receive responds. " << std::endl;
-			close();
-		}
-	}
+        if (bytes_transferred == 3) {
+            std::cout << "[" << m_tid << "]" << "We have received the respond." << std::endl;
+            unsigned char reply_code = m_buf[2];
+            std::cout << "[" << m_tid << "]" << "TCP:" << ((reply_code & 0x01) == 0 ? "Failed." : "Succeeded.") << std::endl;
+            std::cout << "[" << m_tid << "]" << "UDP:" << ((reply_code & 0x02) == 0 ? "Failed." : "Succeeded.") << std::endl;
+            std::cout << "[" << m_tid << "]" << "Socket is closing now." << std::endl;
+            close();
+        } else {
+            std::cout << "[" << m_tid << "]" << "Failed to receive responds. " << std::endl;
+            close();
+        }
+    }
 
-	void send_handler(
-		const boost::system::error_code& error, // Result of operation.
-		std::size_t bytes_transferred           // Number of bytes sent.
-		)
-	{
-		if (error || bytes_transferred == 0)
-		{
-			std::cout << "[" << m_tid << "]" << "error:" << error << "." << error.message() << ". Bytes:" << bytes_transferred << std::endl;
-			close();
-			return;
-		}
-		else
-		{
-			std::cout << "[" << m_tid << "]" << "sent " << bytes_transferred << " Bytes." << std::endl;
-		}
+    void send_handler(
+            const boost::system::error_code& error, // Result of operation.
+            std::size_t bytes_transferred // Number of bytes sent.
+            ) {
+        if (error || bytes_transferred == 0) {
+            std::cout << "[" << m_tid << "]" << "error:" << error << "." << error.message() << ". Bytes:" << bytes_transferred << std::endl;
+            close();
+            return;
+        } else {
+            std::cout << "[" << m_tid << "]" << "sent " << bytes_transferred << " Bytes." << std::endl;
+        }
 
-		if (bytes_transferred == sizeof(m_send_buf))
-		{
-			memset(m_buf, 0, sizeof(m_buf));
-			m_sock->async_receive(boost::asio::buffer(m_buf), boost::bind(&CAsioTCPClient::recv_handler, this, _1, _2));
-			std::cout << "[" << m_tid << "]" << "now waiting for responds." << std::endl;
-		}
-		else
-		{
-			std::cout << "[" << m_tid << "]" << "sent bytes not correct, abort!" << std::endl;
-			close();
-			return;
-		}
-	}
+        if (bytes_transferred == sizeof (m_send_buf)) {
+            memset(m_buf, 0, sizeof (m_buf));
+            m_sock->async_receive(boost::asio::buffer(m_buf), boost::bind(&CAsioTCPClient::recv_handler, this, _1, _2));
+            std::cout << "[" << m_tid << "]" << "now waiting for responds." << std::endl;
+        } else {
+            std::cout << "[" << m_tid << "]" << "sent bytes not correct, abort!" << std::endl;
+            close();
+            return;
+        }
+    }
 
-	void close()
-	{
-		if (m_stoped)
-		{
-			return;
-		}
-		std::cout << "[" << m_tid << "]" << "Client is closing now" << std::endl;
-		if (m_sock)
-		{
-			m_sock->close();
-		}
-		m_stoped = true;
-	}
+    void close() {
+        if (m_stoped) {
+            return;
+        }
+        std::cout << "[" << m_tid << "]" << "Client is closing now" << std::endl;
+        if (m_sock) {
+            m_sock->close();
+        }
+        m_stoped = true;
+    }
 
 private:
-	ip::tcp::endpoint m_ep;
-	io_service& m_io;
-	enum { max_length = 1024 };
-	char m_buf[max_length];
-	char m_send_buf[11] = { 0 };
-	sock_ptr m_sock;
-	bool m_stoped;
-	int m_tid;
+    ip::tcp::endpoint m_ep;
+    io_service& m_io;
+
+    enum {
+        max_length = 1024
+    };
+    char m_buf[max_length];
+    char m_send_buf[11] = {0};
+    sock_ptr m_sock;
+    bool m_stoped;
+    int m_tid;
 };
--- BlockDet/TestClient/TestClient.cpp
+++ BlockDet/TestClient/TestClient.cpp
@@ -5,135 +5,127 @@
 #include "boost/make_shared.hpp"
 
 using boost::asio::ip::udp;
-enum { max_length = 1024 };
 
-class async_udp_server
-{
+enum {
+    max_length = 1024
+};
+
+class async_udp_server {
 public:
-	async_udp_server(boost::asio::io_service& io)
-		: m_io(io)
-	{
-	}
 
-	void run()
-	{
-		m_sock = boost::make_shared<udp::socket>(m_io, udp::endpoint(udp::v4(), 3799));
-		std::cout << "UDP server with port 3799 is running now." << std::endl;
-		boost::shared_ptr<udp::endpoint> sender_endpoint = boost::make_shared<udp::endpoint>();
-		m_sock->async_receive_from(boost::asio::buffer(m_receive_buffer), *sender_endpoint, boost::bind(&async_udp_server::udp_async_recv_from_handler, this, _1, _2, sender_endpoint));
-	}
+    async_udp_server(boost::asio::io_service& io)
+    : m_io(io) {
+    }
+
+    void run() {
+        m_sock = boost::make_shared<udp::socket>(m_io, udp::endpoint(udp::v4(), 3799));
+        std::cout << "UDP server with port 3799 is running now." << std::endl;
+        boost::shared_ptr<udp::endpoint> sender_endpoint = boost::make_shared<udp::endpoint>();
+        m_sock->async_receive_from(boost::asio::buffer(m_receive_buffer), *sender_endpoint, boost::bind(&async_udp_server::udp_async_recv_from_handler, this, _1, _2, sender_endpoint));
+    }
 protected:
 private:
-	void udp_async_recv_from_handler(
-		const boost::system::error_code& error, // Result of operation.
-		std::size_t bytes_transferred,           // Number of bytes received.
-		boost::shared_ptr<udp::endpoint> sender_endpoint
-		)
-	{
-		std::cout << "UDP data received " << bytes_transferred << " bytes" << std::endl;
-		char send_buf[3] = { 0,1,3 };
-		std::cout << "Send UDP data 013" << std::endl;
-		m_sock->async_send_to(boost::asio::buffer(send_buf), *sender_endpoint,
-			boost::bind(&async_udp_server::udp_async_send_to_handler, this, _1, _2, sender_endpoint));
-	}
 
-	void udp_async_send_to_handler(
-		const boost::system::error_code& error, // Result of operation.
-		std::size_t bytes_transferred,           // Number of bytes sent.
-		boost::shared_ptr<udp::endpoint> sender_endpoint
-		)
-	{
-		boost::shared_ptr<udp::endpoint> s = boost::make_shared<udp::endpoint>();
-		m_sock->async_receive_from(boost::asio::buffer(m_receive_buffer), *s, boost::bind(&async_udp_server::udp_async_recv_from_handler, this, _1, _2, s));
-	}
+    void udp_async_recv_from_handler(
+            const boost::system::error_code& error, // Result of operation.
+            std::size_t bytes_transferred, // Number of bytes received.
+            boost::shared_ptr<udp::endpoint> sender_endpoint
+            ) {
+        std::cout << "UDP data received " << bytes_transferred << " bytes" << std::endl;
+        char send_buf[3] = {0, 1, 3};
+        std::cout << "Send UDP data 013" << std::endl;
+        m_sock->async_send_to(boost::asio::buffer(send_buf), *sender_endpoint,
+                boost::bind(&async_udp_server::udp_async_send_to_handler, this, _1, _2, sender_endpoint));
+    }
+
+    void udp_async_send_to_handler(
+            const boost::system::error_code& error, // Result of operation.
+            std::size_t bytes_transferred, // Number of bytes sent.
+            boost::shared_ptr<udp::endpoint> sender_endpoint
+            ) {
+        boost::shared_ptr<udp::endpoint> s = boost::make_shared<udp::endpoint>();
+        m_sock->async_receive_from(boost::asio::buffer(m_receive_buffer), *s, boost::bind(&async_udp_server::udp_async_recv_from_handler, this, _1, _2, s));
+    }
 private:
-	boost::asio::io_service& m_io;
-	boost::shared_ptr<udp::socket> m_sock;
-	char m_receive_buffer[max_length];
+    boost::asio::io_service& m_io;
+    boost::shared_ptr<udp::socket> m_sock;
+    char m_receive_buffer[max_length];
 };
 
 typedef ip::tcp::socket tcp_socket_type;
 typedef boost::shared_ptr<tcp_socket_type> tcp_socket_ptr;
 
 // this class is used to test `pingpong` task
-class async_tcp_server
-{
+
+class async_tcp_server {
 public:
-	async_tcp_server(boost::asio::io_service& io)
-		: m_io(io)
-		, m_acceptor(m_io, ip::tcp::endpoint(ip::tcp::v4(), 3799))
-	{
-		accept();
-	}
-	~async_tcp_server() { ; }
+
+    async_tcp_server(boost::asio::io_service& io)
+    : m_io(io)
+    , m_acceptor(m_io, ip::tcp::endpoint(ip::tcp::v4(), 3799)) {
+        accept();
+    }
+
+    ~async_tcp_server() {
+        ;
+    }
 
 protected:
 private:
-	void accept()
-	{
-		tcp_socket_ptr sock = boost::make_shared<tcp_socket_type>(m_io);
-		m_acceptor.async_accept(*sock, boost::bind(&async_tcp_server::accept_handler, this, _1, sock));
-	}
 
-	void accept_handler(const boost::system::error_code& ec, tcp_socket_ptr sock)
-	{
-		if (ec)
-		{
-			return;
-		}
-		printf("SOCKET: %u Connected.\n", (unsigned int)sock->native_handle());
-		// we do not need to do anything in `pingpong` task because we just test whether it can be connected or not. Just close it after connection was built.
-		boost::system::error_code ec_unused;
-		sock->shutdown(socket_base::shutdown_both, ec_unused);
-		sock->close(ec_unused);
-		printf("Socket is closed.");
-		accept();
-	}
+    void accept() {
+        tcp_socket_ptr sock = boost::make_shared<tcp_socket_type>(m_io);
+        m_acceptor.async_accept(*sock, boost::bind(&async_tcp_server::accept_handler, this, _1, sock));
+    }
+
+    void accept_handler(const boost::system::error_code& ec, tcp_socket_ptr sock) {
+        if (ec) {
+            return;
+        }
+        printf("SOCKET: %u Connected.\n", (unsigned int) sock->native_handle());
+        // we do not need to do anything in `pingpong` task because we just test whether it can be connected or not. Just close it after connection was built.
+        boost::system::error_code ec_unused;
+        sock->shutdown(socket_base::shutdown_both, ec_unused);
+        sock->close(ec_unused);
+        printf("Socket is closed.");
+        accept();
+    }
 private:
-	boost::asio::io_service& m_io;
-	ip::tcp::acceptor m_acceptor;
+    boost::asio::io_service& m_io;
+    ip::tcp::acceptor m_acceptor;
 };
 
 boost::shared_ptr<CAsioTCPClient> cc[99];
 
-void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it, io_service& io)
-{
-	for (int i = 0; i != 99; i++)
-	{
-		if (!ec)
-		{
-			std::cout << "resolved ip: " << it->endpoint().address().to_string() << std::endl;
-			cc[i] = boost::make_shared<CAsioTCPClient>(io, it->endpoint().address(),i);
-			cc[i]->start();
-		}
-		else
-		{
-			std::cout << "Failed to resolve." << std::endl;
-		}
-	}
+void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it, io_service& io) {
+    for (int i = 0; i != 99; i++) {
+        if (!ec) {
+            std::cout << "resolved ip: " << it->endpoint().address().to_string() << std::endl;
+            cc[i] = boost::make_shared<CAsioTCPClient>(io, it->endpoint().address(), i);
+            cc[i]->start();
+        } else {
+            std::cout << "Failed to resolve." << std::endl;
+        }
+    }
 }
 
-int main()
-{
-	try
-	{
-		io_service io;
-		std::cout << "tcp client start." << std::endl;
-		boost::asio::ip::tcp::resolver resolver(io);
-		boost::asio::ip::tcp::resolver::query query("pe.joy189.com", "8888");
-		resolver.async_resolve(query, boost::bind(resolve_handler, _1, _2, boost::ref(io)));
+int main() {
+    try {
+        io_service io;
+        std::cout << "tcp client start." << std::endl;
+        boost::asio::ip::tcp::resolver resolver(io);
+        boost::asio::ip::tcp::resolver::query query("pe.joy189.com", "8888");
+        resolver.async_resolve(query, boost::bind(resolve_handler, _1, _2, boost::ref(io)));
 
-		async_udp_server udp_server(io);
-		async_tcp_server tcps_server(io);
-		udp_server.run();
-		io.run();
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	system("pause");
-	return 0;
+        async_udp_server udp_server(io);
+        async_tcp_server tcps_server(io);
+        udp_server.run();
+        io.run();
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+    system("pause");
+    return 0;
 }
 
 
